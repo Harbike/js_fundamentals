f9e8ef3936ee4c46c40dfb8a8a5fd0b4844a62b3
harbike <adenlekehny@gmail.com>
2024-11-17 09:50:57 +0100

    modified - fetching of modal contents(json)

--- modal_task/modal.js
+++ modal_task/modal.js
@@ -1,91 +1,49 @@
 "use strict";
 
-// targeting individually
-// const modalBtns = document.querySelectorAll('.option');
-// const modalPanes = document.querySelectorAll('.modal');
-// const closeBtns = document.querySelectorAll('.modal_close-btn');
-// const blurPane = document.querySelector('.blurry');
-
-// for (let i = 0; i < modalBtns.length; i++) {
-//     const modalBtn = modalBtns[i];
-//     const pane = modalPanes[i];
-
-//     modalBtn.addEventListener('click', ()=> {
-//         if (modalBtn.value === 'a') {
-//             console.log(`help`);
-//             pane.classList.remove('hidden');
-//         }
-//         if (modalBtn.value === 'b') {
-//             console.log(`break`);
-//             pane.classList.remove('hidden');
-//         }
-//         if (modalBtn.value === 'c') {
-//             console.log(`settled`);
-//             pane.classList.remove('hidden');
-//         }
-
-//     })
-
-//     closeBtns.forEach(close => {
-//         close.addEventListener('click', ()=> {
-//             pane.classList.add('hidden');
-//         })
-//     })
-
-//     // to close pane when another btn is clicked
-//     // modalBtn.addEventListener('focusout', ()=> {
-//         //     pane.classList.add('hidden');
-//         // })
-
-//         // NOTE: highlighting text closes pane!!!!!!!!!!!!!
-// }
-
-
-// textContent route
-
-const modalBtns = document.querySelectorAll('.option');
-const modal = document.querySelector('.modal');
-const heading = document.querySelector('h1');
-const paragraph = document.querySelector('.paragraph');
-const closeBtn = document.querySelector('.modal_close-btn');
-const blurPane = document.querySelector('.blurry');
-
-if (blurPane) {
-    blurPane.classList.add('hidden');
-}
-modalBtns.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        if (btn.value === 'a') {
-            console.log(`yo`);
-            modal.classList.remove('hidden')
-            heading.textContent = 'Yo Model_A!'
-            paragraph.textContent = 'RAt houses the catelit. Maiores, minus quo totam soluta, autem quis ducimus sunt praesentium dolorum ut qui maxime corporis expedita'
-        }
-        if (btn.value === 'b') {
-            console.log(`yolla`);
-            modal.classList.remove('hidden')
-            heading.textContent = 'Yo Model_B!'
-            paragraph.textContent = 'Oraesentium dolorum ut qui maxime corporis expedita, raesentium dolorum ut qui maxime corporis exp!!'
-        }
-        if (btn.value === 'c') {
-            console.log(`yonda`);
-            modal.classList.remove('hidden')
-            heading.textContent = 'Yo Model_C!'
-            paragraph.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nam vitae asperiores fugiat, molestias beatae adipisci ex maxime aliquam rerum numquam enim provident nobis!'
-            console.log(`yolla`);
-        }
-    })
-})
-
-// close pane
-closeBtn.addEventListener('click', ()=> {
-    modal.classList.add('hidden')
-})
-
+const modal = document.querySelector(".modal_container");
+const modalBtns = document.querySelectorAll(".option");
+const heading = document.querySelector(".heading");
+const paragraph = document.querySelector(".paragraph");
+const closeBtn = document.querySelector(".modal_close-btn");
+const blurPane = document.querySelector(".blurry");
 
 // others
-fetch('report.json')
-.then(response => response.json())
-.then(report => {
-    console.log(report);
-})
+
+let output = "";
+fetch("report.json")
+  .then((response) => response.json())
+  .then((report) => {
+    // console.log(report);
+    modalBtns.forEach((btn, index, paneContent, closePane) => {
+      paneContent = report[index];
+
+      btn.addEventListener("click", () => {
+        console.log(`help`); 
+
+        output = `
+        <div class="blurry"></div>
+
+        <div class="modal">
+            <button class="modal_close-btn">&times;</button>
+            <h1 class="heading">${paneContent.heading} </h1>
+            <p class="paragraph">${paneContent.paragraph}</p>
+        </div>
+        `;
+        modal.innerHTML = output;
+
+        closePane = document.querySelector(".modal_close-btn");
+        closePane.addEventListener('click', () => {
+            modal.innerHTML = '';
+        // console.log(`done`);
+
+        });
+      });
+    });
+  });
+
+
+//   challenges
+//  at the output stage, notice usage of += keeps existing pane, causing closePane to malfunction
+
+// closePane = document.querySelector(".modal_close-btn") >>> setting closePane = closeBtn fails, having same call target '.modal_close-btn'
+// thoughtProc -- '.modal_close-btn' no longer exist outside fetch
